f3511124bf2ebcf9341f0b5b30a01d4327f2c128
Ana Maria Sosa <anamariasosam@gmail.com>
2017-05-23 09:40:59 -0500

    remove app style

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons/css/tachyons.css'
-import './styles/App.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
