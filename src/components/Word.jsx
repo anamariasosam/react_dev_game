d02221d1edb3c0aa5b8599723095b5dc3986c498
Ana Maria Sosa <anamariasosam@gmail.com>
2017-05-24 17:27:38 -0500

    Edit founded words color

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Word = ({ word }) => (
-  <li className="dib mh2 mb2 bg-blue shadow-4">
+  <li className="dib mh2 mb2 bg-hot-pink shadow-4">
     <p className="f6 f5-ns db tc white ph3">
       {word.toUpperCase()}
     </p>
